bd9eeb49d1136c663a64dbf16ffbda26f6f7a8fe
Stefan Karlsson <phtefan.karlsson@gmail.com>
2023-05-19 17:11:48 +0200

    Add SearchService and SearchController

--- src/main/java/com/example/searchservice4/SearchService.java
+++ src/main/java/com/example/searchservice4/SearchService.java
@@ -1,12 +1,33 @@
 package com.example.searchservice4;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+//TODO
+// Schema of the Search Service could be like this:
+// 1. Controller with two methods searchByHashtag(string hashtagToSearch) and searchByText(string textToSearch)
+// 2. Entity for the messages
+// 3. Repository that connects to MongoDB with two methods to retrieve messages and another to save messages
+// 4. A service that consumes messages from RabbitMQ and stores them in MongoDB
+
+@Service
 public class SearchService {
-    //TODO
-    // Schema of the Search Service could be like this:
-    // 1. Controller with two methods searchByHashtag(string hashtagToSearch) and searchByText(string textToSearch) DONE
-    // 2. Entity for the messages
-    // 3. Repository that connects to ES or MongoDB with two methods to retrieve messages and another to save messages
-    // 4. A service that consumes messages from RabbitMQ and stores them in ES or MongoDB
+    private final MessageRepository messageRepository;
+
+    public SearchService(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    public List<Message> searchByHashtag(String hashtagToSearch) {
+        return null;
+    }
+
+    public List<Message> searchByText(String textToSearch) {
+        return messageRepository.findByContentContaining(textToSearch);
+
+    }
 
 
 
