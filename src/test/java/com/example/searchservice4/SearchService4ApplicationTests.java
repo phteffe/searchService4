edade79c73333204e7336d8f6a319f9100d9e849
Stefan Karlsson <phtefan.karlsson@gmail.com>
2023-05-19 16:26:01 +0200

    Initial commit

---
+++ src/test/java/com/example/searchservice4/SearchService4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.searchservice4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchService4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
